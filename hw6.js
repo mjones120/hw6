5d5387fca54b0ec3901ad4bd3135a048de070c31
mjones120 <157424025+mjones120@users.noreply.github.com>
2024-03-01 12:21:53 -0500

    Add files via upload

---
+++ hw6.js
@@ -0,0 +1,10 @@
+//1. What is HTTP? What are the different HTTP request methods and how do they differ?
+// HTTP is a hypertext transfer protocol which helps the browser retrieve resources 
+//Methods: GET, POST, PUT, DELETE, PATCH
+// GET retrieeves data from the server. PUT replaces all current representations of a resource with a request payload. POST creates an entity on the server. DELETE deletes a resource. PATCH modifies a resource
+//2. What is an API? What is JSON?
+// API application programming interface allows for more than one computer program to communicate with one another
+//JSON is javascript object notation which allows for client server communication to take place as it passes the data from browser to server and vice versa
+//3. What is AJAX? What is meant by asynchronous code?
+//AJAX is asynchronous javascript and xml that allows for requests to be made without having to refresh the page. This is asynchronous as you can do it without having to refresh the page.
+
